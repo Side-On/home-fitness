0a56425fb203c1960ca7d255117742ef9bc6381d
Side-On <nathanhighams@gmail.com>
2021-08-04 20:37:55 +0000

    Add items models

--- items/models.py
+++ items/models.py
@@ -4,7 +4,25 @@ from django.db import models
 
 
 class Category(models.Model):
+
     name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_name(self):
+        return self.name
+
+
+class Item(models.Model):
+
+    name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    description = models.TextField(blank=True)
+    size = models.CharField(max_length=25, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
