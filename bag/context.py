cf628b6d129b78eca6421818ab5e0a562f036218
Side-On <nathanhighams@gmail.com>
2021-08-16 17:20:04 +0000

    Add context.py for the bag content and added to settings templates to use across apps

---
+++ bag/context.py
@@ -0,0 +1,16 @@
+
+def bag_content(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    grand_total = total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'grand_total': grand_total,
+    }
+
+    return context
