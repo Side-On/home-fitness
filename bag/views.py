634d78184de8b6ec0db8ffb63b7a3ce1e55fa267
Side-On <nathanhighams@gmail.com>
2021-08-17 17:06:26 +0000

    Remove commented code and added line at the end of the file

--- bag/views.py
+++ bag/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def view_bag(request):
+
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -20,5 +20,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
