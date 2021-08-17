acf91195a781496d1c891cb8fe8006b457dcc9bd
Side-On <nathanhighams@gmail.com>
2021-08-17 21:09:11 +0000

    Add spacing and formatting to files

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,7 +20,7 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-            
+
         else:
             product = get_object_or_404(Product, pk=item_id)
             for weight, quantity in item_data['items_by_weight'].items():
