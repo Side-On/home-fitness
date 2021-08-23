e556fc0cc6896e7d9a43ed3e26fd5a78a0ce7e6b
Side-On <nathanhighams@gmail.com>
2021-08-23 10:46:01 +0000

    Added forms, admin, and signals

--- checkout/models.py
+++ checkout/models.py
@@ -23,7 +23,7 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
-        
+
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
@@ -50,6 +50,6 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-        
+
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
