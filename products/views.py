8afa86e5324a1b45d99cbef60502f85fc452bda6
Side-On <nathanhighams@gmail.com>
2021-09-02 21:26:26 +0000

    Add delete product functionality, and links visible to superusers

--- products/views.py
+++ products/views.py
@@ -73,9 +73,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Succesfully added the product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add the product. Please check that the form is valid')
     else:
@@ -100,7 +100,7 @@ def edit_product(request, product_id):
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to update the product. Please make sure that the form is valid')
-    else:    
+    else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
 
@@ -111,3 +111,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
