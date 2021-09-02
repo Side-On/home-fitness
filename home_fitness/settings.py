0c70b948718c0dee0360c5462b493fb8d527002e
Side-On <nathanhighams@gmail.com>
2021-09-02 23:26:05 +0000

    Fix issue with the secret key displaying on the settings

--- home_fitness/settings.py
+++ home_fitness/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9(y*62b5w3(7@d*50^hsrz!abbax(ju!*ebhs&9c=0jnrua=i5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
