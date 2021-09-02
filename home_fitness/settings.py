8ea577709c82b26fd64ae8d7c19b7bddabdfe6f6
Side-On <nathanhighams@gmail.com>
2021-09-02 23:40:37 +0000

    Remove commented code

--- home_fitness/settings.py
+++ home_fitness/settings.py
@@ -121,13 +121,6 @@ WSGI_APPLICATION = 'home_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABSE_URL'))
