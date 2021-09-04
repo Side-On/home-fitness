2041b01c793f5e497393acf6ea6af8125c7318f7
Side-On <nathanhighams@gmail.com>
2021-09-04 13:30:08 +0000

    Fix type to send real emails

--- home_fitness/settings.py
+++ home_fitness/settings.py
@@ -209,7 +209,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'homefitness@project.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
