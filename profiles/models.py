2ba51c83052751cd62ee7da5edf13ce61d676160
Side-On <nathanhighams@gmail.com>
2021-09-05 13:16:50 +0000

    Add address model to start testing multiple delivery addresses

--- profiles/models.py
+++ profiles/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -26,3 +27,18 @@ def create_or_update_user_profile(sender, instance, created, **kargs):
     if created:
         UserProfile.objects.create(user=instance)
     instance.userprofile.save()
+
+
+class Address(models.Model):
+
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=False, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country', null=True, blank=True)
+
+    def __str__(self):
+        return self.address
