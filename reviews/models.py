09b759f25856f88f738016e221dc75b51e3b272a
Side-On <nathanhighams@gmail.com>
2021-09-06 15:52:46 +0000

    Create review app and model

---
+++ reviews/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+# Create your models here.
+
+
+class Review(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review_title = models.CharField(max_length=254, null=False, blank=False)
+    review_comment = models.TextField(max_length=800, null=False, blank=False)
+
+    def __str__(self):
+        return f'Review made by {self.user} about the {self.product.name}'
+
