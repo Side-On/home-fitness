06b931c809324f19a8d43c0f5e4e16904f3972f2
Side-On <nathanhighams@gmail.com>
2021-09-08 21:50:37 +0000

    Test stories app rendering

--- home_fitness/urls.py
+++ home_fitness/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
     path('fit_tips/', include('fit_tips.urls')),
+    path('stories/', include('stories.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from profiles.models import User
+from stories.models import Story
 
 # Create your views here.
+
+
+def all_stories(request):
+
+    stories = Story.objects.all()
+
+    context = {
+        'stories': stories,
+    }
+    return render(request, 'stories/stories.html', context)
