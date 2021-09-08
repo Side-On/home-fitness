1e8f8d8e1393e13e4286d257dbfa984485c21d44
Side-On <nathanhighams@gmail.com>
2021-09-08 16:58:16 +0000

    Add model for fit tips and migrate

--- fit_tips/models.py
+++ fit_tips/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
 # Create your models here.
+
+class FitTip(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="fit_tips")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fit_tip_title = models.CharField(max_length=100, null=False, blank=False)
+    fit_tip = models.TextField(max_length=400, null=False, blank=False)
+
+    def __str__(self):
+        return f'Fit tip left by {self.user} on {self.product}'
